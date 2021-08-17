805ccd89a07946c566ff03ba7bb4c3626c9a9cc9
David Bootle <davidtbootle@gmail.com>
2021-08-17 14:30:41 -0400

    added verbose option

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 class DuplicateRemover:
 
-    def __init__(self, source_path: str, no_inputs = False, generate_logs = False, log_path = None):
+    def __init__(self, source_path: str, no_inputs = False, generate_logs = False, log_path = None, verbose = False):
         # validate
         log_path_set = True if log_path else False
         if not log_path:
@@ -18,10 +18,13 @@ class DuplicateRemover:
 
         # set properties
         self.source_path = source_path
+
+        ## config properties
         self.no_inputs = no_inputs
         self.generate_logs = generate_logs
         self.log_path = log_path_obj
         self.log_path_set = log_path_set
+        self.verbose = verbose
 
     def welcome(self):
         print()
@@ -58,6 +61,7 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--no-inputs', help="skips all inputs, assuming the default value", action='store_true')
     parser.add_argument('-q', '--quiet', help="the app will not generate log files", action="store_true")
     parser.add_argument('-o', '--output', help="the path of the log output file", type=str)
+    parser.add_argument('-v', '--verbose', help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
     source_path = args.path
@@ -67,5 +71,6 @@ if __name__ == '__main__':
         source_path,
         no_inputs=args.no_inputs,
         generate_logs=(not args.quiet),
-        log_path=args.output)
+        log_path=args.output,
+        verbose=args.verbose)
     App.welcome()
